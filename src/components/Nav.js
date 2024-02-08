a68a409e2393af85fdfbc90710f610e89bd79d4f
Patricia Muresan <patriciamariamrsn@gmail.com>
2024-02-08 13:16:48 -0500

    created the Navbar

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+function Nav() {
+    return (
+        <nav>
+            <h1>taste the joy</h1>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Recipes</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav
