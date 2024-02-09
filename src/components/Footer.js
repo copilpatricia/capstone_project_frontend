1d85012787fbe7eb6a2182325b063c999d603332
Patricia Muresan <patriciamariamrsn@gmail.com>
2024-02-09 14:58:28 -0500

    worked on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,15 @@
 function Footer() {
   return (
-    <>
+    <div className="super_container">
       <div className="footer_container">
         <i className="fa-brands fa-square-instagram fa-2xl"></i>
         <i className="fa-brands fa-pinterest fa-2xl"></i>
         <i className="fa-brands fa-facebook fa-2xl"></i>
         <i className="fa-brands fa-twitter fa-2xl"></i>
-        <div className="subfooter_container">
-          <p>
-            © 2024 Taste of joy. All Rights Reserved. A Raptive Partner Site.
-          </p>
-        </div>
       </div>
-    </>
+      <p style={{textAlign: "center"}}>© 2024 Taste of joy. All Rights Reserved. A Raptive Partner Site.</p>
+    </div>
   );
 }
 
 export default Footer;
-
