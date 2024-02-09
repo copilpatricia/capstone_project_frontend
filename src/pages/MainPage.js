17d9ae481fc08dd64a540ea9603094487f8aac25
Patricia Muresan <patriciamariamrsn@gmail.com>
2024-02-08 19:54:56 -0500

    styled the page

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,9 +1,14 @@
 function MainPage({ quote }) {
   return (
     <div className="container_main">
-      <h2 id="title_MP">SIMPLE RECIPES MADE FOR  everyday life.</h2>
+      <h2 id="title_MP">
+        SIMPLE RECIPES MADE FOR
+        <span id="word_MP">real. actual. everyday life.</span>
+      </h2>
       <section className="quote_section">
+        <div className="quote_container">
           <p id="p_MP">{quote.sentence}</p>
+        </div>
       </section>
     </div>
   );
