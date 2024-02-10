1a244d33ecbe855a2d6f21980b804ae2702628ed
Patricia Muresan <patriciamariamrsn@gmail.com>
2024-02-10 16:46:39 -0500

    signuser component

--- src/pages/SignUser.js
+++ src/pages/SignUser.js
@@ -1,10 +1,27 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 function SignUser() {
   const emailInputRef = useRef(null);
   const passwordInputRef = useRef(null);
+  const [showSignUp, setShowSignUp] = useState(false);
 
-  const handleSubmit = (e) => {
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    console.log(emailInputRef.current.value);
+    console.log(passwordInputRef.current.value);
+
+    if (emailInputRef.current.value === "") {
+        emailInputRef.current.focus();
+        return;
+      }
+      if (passwordInputRef.current.value === "") {
+        passwordInputRef.current.focus();
+        return;
+      }
+    
+  };
+
+  const handleSignUp = (e) => {
     e.preventDefault();
     console.log(emailInputRef.current.value);
     console.log(passwordInputRef.current.value);
@@ -20,48 +37,84 @@ function SignUser() {
     
   };
   return (
-    <>
-      <form onSubmit={handleSubmit} className="form_container">
-        <h3>Sign in</h3>
-        <label htmlFor="email">Email</label>
-        <input
-          ref={emailInputRef}
-          name="email"
-          id="email"
-          type="text"
-          placeholder="Email"
-        />
-        <br />
-        <label htmlFor="password">Password</label>
-        <input
-          ref={passwordInputRef}
-          name="password"
-          id="password"
-          type="text"
-          placeholder="Password"
-        />
-        <button type="submit">Sign In</button>
-      </form>
-      <span>
-        Don't have an account? <button>Sign Up</button>
-      </span>
-    </>
+    <main>
+
+      {showSignUp ? (
+        <div>
+          <form
+            className="form_container"
+            onSubmit={handleSignIn}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              padding: "10px",
+            }}
+          >
+            <h3>Sign In</h3>
+            <label htmlFor="email">Email</label>
+            <input
+              ref={emailInputRef}
+              name="email"
+              id="email"
+              type="text"
+              placeholder="Email or phone"
+            />
+            <label htmlFor="password">Password</label>
+            <input
+              ref={passwordInputRef}
+              name="password"
+              id="password"
+              type="text"
+            />
+            <button type="submit">Sign In</button>
+          </form>
+          <span>
+            Don't have an account?{" "}
+            <button onClick={() => setShowSignUp(!showSignUp)}>Sign Up</button>
+          </span>
+        </div>
+      ) : (
+        <div>
+          <form
+          className="form_container"
+            onSubmit={handleSignUp}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              padding: "10px",
+            }}
+          >
+            <h3>Sign Up</h3>
+            <label htmlFor="email">Email</label>
+            <input
+              ref={emailInputRef}
+              name="email"
+              id="email"
+              type="text"
+              placeholder="Email or phone"
+            />
+            <label htmlFor="password">Password</label>
+            <input
+              ref={passwordInputRef}
+              name="password"
+              id="password"
+              type="text"
+            />
+            <button type="submit">Sign Up</button>
+          </form>
+          <span>
+            Already have an account?{" "}
+            <button onClick={() => setShowSignUp(!showSignUp)}>Sign In</button>
+          </span>
+        </div>
+      )}
+    </main>
   );
 }
 
 export default SignUser;
 
-// {
-  /* <br />
-      <form className="form_container">
-        <h3>Sign up</h3>
-        <label htmlFor="email">Email</label>
-        <input  name="email" id="email" type="text" />
-        <br />
-        <label htmlFor="password">Password</label>
-        <input name="password" id="password" type="text" />
-      </form>
-      <span>
-        Don't have an account? <button>Sign Up</button>
-      </span> */
-// }
+
+
+// no control form = useRef - for this form doesn't need to react with what the user is going to type 
+// control form = useState - when the user type React knows that something is going to change - starting to filter
